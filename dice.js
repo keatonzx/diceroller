efc3076ec46bf1adb86427cb367be8d44a91f2ab
Keaton Armstrong <keaton@Keatons-MacBook-Pro.local>
2018-02-22 15:18:29 -0800

    redid everything basically

--- dice.js
+++ dice.js
@@ -1,23 +1,38 @@
 var sides = 6;
 var numberdice = 5;
+var dicerolled = [numberdice];
 
 document.getElementById('sides').innerHTML = sides;
 document.getElementById('numberdice').innerHTML = numberdice;
 
-rolldice(numberdice, sides);
+
+function dicearr(){
+  for (var i = 0; i < numberdice; i++){
+    dicerolled[i] = rollside(sides+1);
+  }
+}
 
 function rolldice(numberdice, sides) {
-	var total = 0;
-
 	for (var i = 0; i < numberdice; i++) {
 		var di = rollside(sides + 1);
-		total += di;
-    document.getElementById("mydice").innerHTML += ('Dice ' + (i+1) + ' = ' + di+" ");
 	}
-  document.getElementById("total").innerHTML = ("Total: "+ total);
-
 }
 
 function rollside(sides) {
 	return Math.floor(Math.random() * sides);
+}
+
+function myCreateFunction() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = dicerolled[0];
+    cell2.innerHTML = dicerolled[1];
+    cell3.innerHTML = dicerolled[0]+dicerolled[1];
+}
+
+function myDeleteFunction() {
+    document.getElementById("myTable").deleteRow(0);
 }
